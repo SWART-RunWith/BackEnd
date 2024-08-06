377ff0cdcd1ee46c5b24cdf39973dda28d6d39e1
shin0112 <wnslcosltimo12@gmail.com>
2024-08-06 15:26:56 +0900

    chore : init setting

---
+++ src/main/java/com/swart/runwith/RunwithApplication.java
@@ -0,0 +1,13 @@
+package com.swart.runwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunwithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swart/runwith/RunwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swart.runwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
