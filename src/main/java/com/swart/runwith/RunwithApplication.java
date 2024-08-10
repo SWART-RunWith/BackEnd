017694892246fe7a11cc176ca689c52b822fc026
shin0112 <wnslcosltimo12@gmail.com>
2024-08-11 00:54:25 +0900

    chore : security 방지

--- src/main/java/com/swart/runwith/RunwithApplication.java
+++ src/main/java/com/swart/runwith/RunwithApplication.java
@@ -2,12 +2,13 @@ package com.swart.runwith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RunwithApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RunwithApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RunwithApplication.class, args);
+    }
 
 }
