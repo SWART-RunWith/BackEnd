8092040206e54d6a179f47dc7c5e552a65f05f6a
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 02:31:22 +0900

    feature : service - sign up 틀 작성

--- src/main/java/com/swart/runwith/domain/user/service/UserService.java
+++ src/main/java/com/swart/runwith/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.swart.runwith.domain.user.service;
 
+import com.swart.runwith.domain.user.dto.service.UserCreateServiceRequestDto;
+
 public interface UserService {
 
+    void signup(UserCreateServiceRequestDto serviceRequestDto);
 }
--- src/main/java/com/swart/runwith/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/swart/runwith/domain/user/service/impl/UserServiceImpl.java
@@ -1,11 +1,30 @@
 package com.swart.runwith.domain.user.service.impl;
 
+import com.swart.runwith.domain.user.dto.service.UserCreateServiceRequestDto;
+import com.swart.runwith.domain.user.repository.AuthRepository;
+import com.swart.runwith.domain.user.repository.UserRepository;
 import com.swart.runwith.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final AuthRepository authRepository;
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public void signup(final UserCreateServiceRequestDto serviceRequestDto) {
+        // 휴대전화 중복 검사
+
+        // 이메일 중복 검사
+
+        // 회원 정보 및 회원 계정 저장
+    }
 }
