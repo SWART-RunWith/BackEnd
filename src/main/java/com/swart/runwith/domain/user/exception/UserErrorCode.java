a303af492844dffe46864c0399ba14e44974ef8d
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 03:26:49 +0900

    feature : 로그인 error code 작성

--- src/main/java/com/swart/runwith/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/swart/runwith/domain/user/exception/UserErrorCode.java
@@ -1,5 +1,7 @@
 package com.swart.runwith.domain.user.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import com.swart.runwith.global.exception.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +10,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    DUPLICATE_PHONE(HttpStatus.BAD_REQUEST, "이미 존재하는 전화번호입니다."),
-    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    // 400
+    DUPLICATE_PHONE(BAD_REQUEST, "이미 존재하는 전화번호입니다."),
+    DUPLICATE_EMAIL(BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    INVALID_EMAIL(BAD_REQUEST, "잘못된 않는 이메일입니다."),
+    INVALID_PASSWORD(BAD_REQUEST, "잘못된 비밀번호입니다."),
     ;
 
     private final HttpStatus status;
