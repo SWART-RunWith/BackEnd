2e620697f35da8263b73b589719d9ec487d417fa
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 02:41:38 +0900

    feature : 검증을 위한 find 메서드 작성
    - user repository : find by phone
    - auth repository : find by email

--- src/main/java/com/swart/runwith/domain/user/repository/AuthRepository.java
+++ src/main/java/com/swart/runwith/domain/user/repository/AuthRepository.java
@@ -1,8 +1,10 @@
 package com.swart.runwith.domain.user.repository;
 
 import com.swart.runwith.domain.user.entity.Auth;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthRepository extends JpaRepository<Auth, Long> {
 
+    Optional<Auth> findByEmail(String email);
 }
--- src/main/java/com/swart/runwith/domain/user/repository/UserRepository.java
+++ src/main/java/com/swart/runwith/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.swart.runwith.domain.user.repository;
 
 import com.swart.runwith.domain.user.entity.UserInfo;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserInfo, Long> {
 
+    Optional<UserInfo> findByPhone(String phone);
 }
