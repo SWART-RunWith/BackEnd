a9616c96b2f0c9266848bb13075580e881a45ad2
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 04:17:52 +0900

    feature : 에러 코드 추가

--- src/main/java/com/swart/runwith/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/swart/runwith/domain/user/exception/UserErrorCode.java
@@ -19,6 +19,7 @@ public enum UserErrorCode implements ErrorCode {
 
     // 404
     NOT_FOUND_USER(NOT_FOUND, "유저를 찾을 수 없습니다"),
+    NOT_FOUND_AUTH(NOT_FOUND, "계정을 찾을 수 없습니다"),
     ;
 
     private final HttpStatus status;
