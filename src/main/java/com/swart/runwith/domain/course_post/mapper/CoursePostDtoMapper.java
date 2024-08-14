e783f2dccc0c93108878a81e17a4276c3cda249f
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 17:58:18 +0900

    feature : course post 작성 mapper

--- src/main/java/com/swart/runwith/domain/course_post/mapper/CoursePostDtoMapper.java
+++ src/main/java/com/swart/runwith/domain/course_post/mapper/CoursePostDtoMapper.java
@@ -2,9 +2,14 @@ package com.swart.runwith.domain.course_post.mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+import com.swart.runwith.domain.course_post.dto.controller.CoursePostCreateControllerRequestDto;
+import com.swart.runwith.domain.course_post.dto.service.CoursePostCreateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
 public interface CoursePostDtoMapper {
 
+    CoursePostCreateServiceRequestDto toCoursePostCreateServiceRequestDto(
+        CoursePostCreateControllerRequestDto controllerRequestDto
+    );
 }
