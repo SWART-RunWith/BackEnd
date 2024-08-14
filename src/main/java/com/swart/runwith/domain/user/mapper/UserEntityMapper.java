a802118db9417c15c0e04af3f38736874e2d3418
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 16:45:08 +0900

    feature : mapper 작성

--- src/main/java/com/swart/runwith/domain/user/mapper/UserEntityMapper.java
+++ src/main/java/com/swart/runwith/domain/user/mapper/UserEntityMapper.java
@@ -3,6 +3,7 @@ package com.swart.runwith.domain.user.mapper;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.swart.runwith.domain.user.dto.service.request.UserCreateServiceRequestDto;
+import com.swart.runwith.domain.user.dto.service.response.UserReadServiceResponseDto;
 import com.swart.runwith.domain.user.entity.UserInfo;
 import org.mapstruct.Mapper;
 
@@ -11,5 +12,9 @@ public interface UserEntityMapper {
 
     UserInfo toUserInfo(
         UserCreateServiceRequestDto serviceRequestDto
+    );
+
+    UserReadServiceResponseDto toUserReadServiceResponseDto(
+        UserInfo userInfo
     );
 }
