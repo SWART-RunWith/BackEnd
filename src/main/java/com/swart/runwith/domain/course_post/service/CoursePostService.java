136dcef3bbab5a1e4475c0198e25c3b33ac5fbfe
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 17:36:42 +0900

    feature : course post 관련 파일 생성

---
+++ src/main/java/com/swart/runwith/domain/course_post/service/CoursePostService.java
@@ -0,0 +1,5 @@
+package com.swart.runwith.domain.course_post.service;
+
+public interface CoursePostService {
+
+}
---
+++ src/main/java/com/swart/runwith/domain/course_post/service/impl/CoursePostServiceImpl.java
@@ -0,0 +1,11 @@
+package com.swart.runwith.domain.course_post.service.impl;
+
+import com.swart.runwith.domain.course_post.service.CoursePostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CoursePostServiceImpl implements CoursePostService {
+
+}
