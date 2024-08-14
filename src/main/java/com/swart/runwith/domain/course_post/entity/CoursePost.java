6de0763d81e3a3bb4c37e5a88419ec380d5ca798
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 17:31:08 +0900

    feature : 코스 글 entity 생성

---
+++ src/main/java/com/swart/runwith/domain/course_post/entity/CoursePost.java
@@ -0,0 +1,53 @@
+package com.swart.runwith.domain.course_post.entity;
+
+import com.swart.runwith.domain.user.entity.UserInfo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CoursePost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    String title;
+    @Column
+    String content;
+    @Column
+    Long like = 0L;
+    // 러닝 데이터 & 코스 추가
+    // @OneToOne
+    // RunningData runningData;
+    @ManyToOne(
+        fetch = FetchType.LAZY,
+        optional = false
+    )
+    UserInfo userInfo;
+
+    @Builder
+    public CoursePost(
+        final String title,
+        final String content,
+        final Long like,
+        final UserInfo userInfo
+    ) {
+        this.title = title;
+        this.content = content;
+        this.like = like;
+        this.userInfo = userInfo;
+    }
+}
