6706e601a2fda671b9ab995a479e9c3a75b0b5e4
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 20:24:53 +0900

    feature : error code 작성

--- src/main/java/com/swart/runwith/domain/course_post/exception/CoursePostErrorCode.java
+++ src/main/java/com/swart/runwith/domain/course_post/exception/CoursePostErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum CoursePostErrorCode implements ErrorCode {
+    // 404
+    NOT_FOUND_COURSE_POST(HttpStatus.NOT_FOUND, "코스 공유 글을 찾을 수 없습니다."),
     ;
     private final HttpStatus status;
     private final String message;
