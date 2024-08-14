1cdd55d8d6771409d2d2746d94aa042db41ebd27
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 17:58:53 +0900

    feature : controller - course post 작성

--- src/main/java/com/swart/runwith/domain/course_post/controller/CoursePostController.java
+++ src/main/java/com/swart/runwith/domain/course_post/controller/CoursePostController.java
@@ -1,6 +1,17 @@
 package com.swart.runwith.domain.course_post.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.swart.runwith.domain.course_post.dto.controller.CoursePostCreateControllerRequestDto;
+import com.swart.runwith.domain.course_post.dto.service.CoursePostCreateServiceRequestDto;
+import com.swart.runwith.domain.course_post.mapper.CoursePostDtoMapper;
+import com.swart.runwith.domain.course_post.service.CoursePostService;
+import com.swart.runwith.domain.user.entity.Auth;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CoursePostController {
 
+    // service
+    private final CoursePostService coursePostService;
+
+    // mapper
+    private final CoursePostDtoMapper coursePostDtoMapper;
+
+    @PostMapping("")
+    public ResponseEntity<?> createCoursePost(
+        Auth userDetails,
+        @Valid @RequestBody CoursePostCreateControllerRequestDto controllerRequestDto
+    ) {
+        CoursePostCreateServiceRequestDto serviceRequestDto =
+            coursePostDtoMapper.toCoursePostCreateServiceRequestDto(controllerRequestDto);
+
+        coursePostService.createCoursePost(
+            userDetails,
+            serviceRequestDto
+        );
+
+        return ResponseEntity
+            .status(CREATED)
+            .build();
+    }
 }
