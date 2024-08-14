9252f42063cfb85e2e8c755d6281741bb75ed7b6
shin0112 <wnslcosltimo12@gmail.com>
2024-08-14 21:04:04 +0900

    feature : find by user info 작성

--- src/main/java/com/swart/runwith/domain/course_post/repository/CoursePostRepository.java
+++ src/main/java/com/swart/runwith/domain/course_post/repository/CoursePostRepository.java
@@ -1,8 +1,11 @@
 package com.swart.runwith.domain.course_post.repository;
 
 import com.swart.runwith.domain.course_post.entity.CoursePost;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoursePostRepository extends JpaRepository<CoursePost, Long> {
 
+    List<CoursePost> findByUserInfo(UserInfo userInfo);
 }
