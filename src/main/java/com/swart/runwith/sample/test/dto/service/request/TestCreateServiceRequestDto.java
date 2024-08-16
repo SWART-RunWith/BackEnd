de195b7aa9177c0169481515f78cc45330ec8848
shin0112 <wnslcosltimo12@gmail.com>
2024-08-16 15:15:59 +0900

    style : 불필요한 notnull 삭제

--- src/main/java/com/swart/runwith/sample/test/dto/service/request/TestCreateServiceRequestDto.java
+++ src/main/java/com/swart/runwith/sample/test/dto/service/request/TestCreateServiceRequestDto.java
@@ -1,13 +1,8 @@
 package com.swart.runwith.sample.test.dto.service.request;
 
-import jakarta.validation.constraints.NotNull;
-
 public record TestCreateServiceRequestDto(
-    @NotNull
     String field,
-    @NotNull
     String title,
-    @NotNull
     String introduction
 ) {
 
