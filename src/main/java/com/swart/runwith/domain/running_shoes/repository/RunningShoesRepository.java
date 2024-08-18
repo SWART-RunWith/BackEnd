211475fcd794ded569e8bf7bbb595e53c31308f0
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:15:50 +0900

    feature : find by userinfo 함수 작성

--- src/main/java/com/swart/runwith/domain/running_shoes/repository/RunningShoesRepository.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/repository/RunningShoesRepository.java
@@ -1,8 +1,11 @@
 package com.swart.runwith.domain.running_shoes.repository;
 
 import com.swart.runwith.domain.running_shoes.entity.RunningShoes;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RunningShoesRepository extends JpaRepository<RunningShoes, Long> {
 
+    List<RunningShoes> findByUserInfo(UserInfo userInfo);
 }
