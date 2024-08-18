bc536cc2d5282c470f1b8319c668f6f7e9b94145
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:07:49 +0900

    feature : read response dto 작성

---
+++ src/main/java/com/swart/runwith/domain/running_shoes/dto/service/response/RunningShoesReadServiceResponseDto.java
@@ -0,0 +1,9 @@
+package com.swart.runwith.domain.running_shoes.dto.service.response;
+
+public record RunningShoesReadServiceResponseDto(
+    String nickname,
+    String brand,
+    String model
+) {
+
+}
