c815fb3b7c7dc35556e58166a0528eb1751e3fbc
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:35:41 +0900

    feature : error code 추가

--- src/main/java/com/swart/runwith/domain/running_shoes/exception/RunningShoesErrorCode.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/exception/RunningShoesErrorCode.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum RunningShoesErrorCode implements ErrorCode {
+    // 403
+    FORBIDDEN_RUNNING_SHOES(HttpStatus.FORBIDDEN, "러닝화 정보 접근 권한이 없습니다"),
+    
+    // 404
     NOT_FOUND_RUNNING_SHOES(HttpStatus.NOT_FOUND, "러닝화 정보를 찾을 수 없습니다"),
     ;
 
