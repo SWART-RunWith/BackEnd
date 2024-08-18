1ad27f9c8d4e5ad8f718658304a4e882f4a5af76
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 02:05:28 +0900

    refactor : read response dto 필드 수정
    id 값 추가

--- src/main/java/com/swart/runwith/domain/running_shoes/dto/service/response/RunningShoesReadServiceResponseDto.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/dto/service/response/RunningShoesReadServiceResponseDto.java
@@ -1,6 +1,7 @@
 package com.swart.runwith.domain.running_shoes.dto.service.response;
 
 public record RunningShoesReadServiceResponseDto(
+    Long id,
     String nickname,
     String brand,
     String model
