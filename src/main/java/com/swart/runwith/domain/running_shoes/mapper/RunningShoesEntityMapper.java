cf7c60e8665634e0abc90764718602cfbd642174
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:12:54 +0900

    feature : entity mapper 작성

--- src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesEntityMapper.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesEntityMapper.java
@@ -3,6 +3,7 @@ package com.swart.runwith.domain.running_shoes.mapper;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.swart.runwith.domain.running_shoes.dto.service.request.RunningShoesCreateServiceRequestDto;
+import com.swart.runwith.domain.running_shoes.dto.service.response.RunningShoesReadServiceResponseDto;
 import com.swart.runwith.domain.running_shoes.entity.RunningShoes;
 import com.swart.runwith.domain.user.entity.UserInfo;
 import org.mapstruct.Mapper;
@@ -13,5 +14,9 @@ public interface RunningShoesEntityMapper {
     RunningShoes toRunningShoes(
         RunningShoesCreateServiceRequestDto serviceRequestDto,
         UserInfo userInfo
+    );
+
+    RunningShoesReadServiceResponseDto toRunningShoesReadServiceResponseDto(
+        RunningShoes runningShoes
     );
 }
