8bff202f741b981cb79c13385aa9a1886a2634d5
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:02:04 +0900

    feature : entity mapper 추가

--- src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesEntityMapper.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesEntityMapper.java
@@ -2,9 +2,16 @@ package com.swart.runwith.domain.running_shoes.mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+import com.swart.runwith.domain.running_shoes.dto.service.request.RunningShoesCreateServiceRequestDto;
+import com.swart.runwith.domain.running_shoes.entity.RunningShoes;
+import com.swart.runwith.domain.user.entity.UserInfo;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
 public interface RunningShoesEntityMapper {
 
+    RunningShoes toRunningShoes(
+        RunningShoesCreateServiceRequestDto serviceRequestDto,
+        UserInfo userInfo
+    );
 }
--- src/main/java/com/swart/runwith/domain/running_shoes/service/impl/RunningShoesServiceImpl.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/service/impl/RunningShoesServiceImpl.java
@@ -1,5 +1,6 @@
 package com.swart.runwith.domain.running_shoes.service.impl;
 
+import com.swart.runwith.domain.running_shoes.mapper.RunningShoesEntityMapper;
 import com.swart.runwith.domain.running_shoes.repository.RunningShoesRepository;
 import com.swart.runwith.domain.running_shoes.service.RunningShoesService;
 import lombok.RequiredArgsConstructor;
@@ -12,4 +13,6 @@ public class RunningShoesServiceImpl implements RunningShoesService {
     // repository
     private final RunningShoesRepository runningShoesRepository;
 
+    // mapper
+    private final RunningShoesEntityMapper runningShoesEntityMapper;
 }
