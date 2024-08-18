eca24b5afd706b08deb94bd10d5d6627af8b35b2
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:01:21 +0900

    feature : controller - 러닝화 생성 로직 작성

--- src/main/java/com/swart/runwith/domain/running_shoes/controller/RunningShoesController.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/controller/RunningShoesController.java
@@ -1,7 +1,14 @@
 package com.swart.runwith.domain.running_shoes.controller;
 
+import com.swart.runwith.domain.running_shoes.dto.controller.RunningShoesCreateControllerRequestDto;
+import com.swart.runwith.domain.running_shoes.dto.service.request.RunningShoesCreateServiceRequestDto;
+import com.swart.runwith.domain.running_shoes.mapper.RunningShoesDtoMapper;
 import com.swart.runwith.domain.running_shoes.service.RunningShoesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,26 @@ public class RunningShoesController {
 
     // service
     private final RunningShoesService runningShoesService;
+
+    // mapper
+    private final RunningShoesDtoMapper runningShoesDtoMapper;
+
+    @PostMapping("")
+    public ResponseEntity<?> create(
+//        @AuthenticationPrincipal UserDetails userDetails,
+        @RequestBody RunningShoesCreateControllerRequestDto controllerRequestDto
+    ) {
+        RunningShoesCreateServiceRequestDto serviceRequestDto =
+            runningShoesDtoMapper.toRunningShoesCreateServiceRequestDto(controllerRequestDto);
+
+        runningShoesService.create(
+//            userDetails,
+            serviceRequestDto
+        );
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .build();
+    }
 
 }
