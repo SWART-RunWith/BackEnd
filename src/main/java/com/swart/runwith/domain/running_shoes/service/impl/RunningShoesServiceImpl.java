afb61efa58cb3a7d1cfba9d524f403a44a8bb8e8
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 01:03:34 +0900

    feature : test용 user info 가져오기

--- src/main/java/com/swart/runwith/domain/running_shoes/service/impl/RunningShoesServiceImpl.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/service/impl/RunningShoesServiceImpl.java
@@ -3,6 +3,10 @@ package com.swart.runwith.domain.running_shoes.service.impl;
 import com.swart.runwith.domain.running_shoes.mapper.RunningShoesEntityMapper;
 import com.swart.runwith.domain.running_shoes.repository.RunningShoesRepository;
 import com.swart.runwith.domain.running_shoes.service.RunningShoesService;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import com.swart.runwith.domain.user.exception.UserErrorCode;
+import com.swart.runwith.domain.user.exception.UserException;
+import com.swart.runwith.domain.user.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +16,14 @@ public class RunningShoesServiceImpl implements RunningShoesService {
 
     // repository
     private final RunningShoesRepository runningShoesRepository;
+    private final AuthRepository authRepository;
 
     // mapper
     private final RunningShoesEntityMapper runningShoesEntityMapper;
+
+    // token 작성 전 test 용 user info 및 auth 반환 method
+    private UserInfo testUserInfo() {
+        return authRepository.findById(1L)
+            .orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND_AUTH)).getUserInfo();
+    }
 }
