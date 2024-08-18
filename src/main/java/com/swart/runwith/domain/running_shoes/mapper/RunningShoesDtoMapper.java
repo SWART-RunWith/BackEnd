7f7641be9872f699124c2b76c25bacbd1715a5f9
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 00:55:50 +0900

    feature : dto mapper 작성

--- src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesDtoMapper.java
+++ src/main/java/com/swart/runwith/domain/running_shoes/mapper/RunningShoesDtoMapper.java
@@ -2,9 +2,15 @@ package com.swart.runwith.domain.running_shoes.mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+import com.swart.runwith.domain.running_shoes.dto.controller.RunningShoesCreateControllerRequestDto;
+import com.swart.runwith.domain.running_shoes.dto.service.request.RunningShoesCreateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
 public interface RunningShoesDtoMapper {
+
+    RunningShoesCreateServiceRequestDto toRunningShoesCreateServiceRequestDto(
+        RunningShoesCreateControllerRequestDto controllerRequestDto
+    );
 
 }
