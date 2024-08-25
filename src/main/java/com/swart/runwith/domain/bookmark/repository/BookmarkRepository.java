a588742a99257c22a3ac659205447e2046a9c0b3
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 23:33:24 +0900

    feature : 북마크 탐색 메서드 작성

--- src/main/java/com/swart/runwith/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/swart/runwith/domain/bookmark/repository/BookmarkRepository.java
@@ -1,8 +1,16 @@
 package com.swart.runwith.domain.bookmark.repository;
 
 import com.swart.runwith.domain.bookmark.entity.Bookmark;
+import com.swart.runwith.domain.course_post.entity.CoursePost;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    Optional<Bookmark> findByUserInfoAndCoursePost(
+        UserInfo userInfo,
+        CoursePost coursePost
+    );
 
 }
