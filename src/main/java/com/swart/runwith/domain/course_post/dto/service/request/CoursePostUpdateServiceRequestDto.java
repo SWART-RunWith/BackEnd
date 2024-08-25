9657acff5156f7def1fd63d807fdddc688f87335
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 19:37:53 +0900

    style : dto 유효성 변경

--- src/main/java/com/swart/runwith/domain/course_post/dto/service/request/CoursePostUpdateServiceRequestDto.java
+++ src/main/java/com/swart/runwith/domain/course_post/dto/service/request/CoursePostUpdateServiceRequestDto.java
@@ -1,10 +1,11 @@
 package com.swart.runwith.domain.course_post.dto.service.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record CoursePostUpdateServiceRequestDto(
-    @NotNull(message = "필수 항목 : title")
+    @NotBlank(message = "필수 항목 : title")
     String title,
+    @NotBlank(message = "필수 항목 : content")
     String content
     // TO DO : 러닝 데이터 id
 ) {
