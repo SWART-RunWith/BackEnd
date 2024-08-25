fdcb976c38d5190d20c5f0e455f0293181346a3c
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 23:07:20 +0900

    feature : 북마크 엔티티 작성

---
+++ src/main/java/com/swart/runwith/domain/bookmark/entity/Bookmark.java
@@ -0,0 +1,45 @@
+package com.swart.runwith.domain.bookmark.entity;
+
+import com.swart.runwith.domain.course_post.entity.CoursePost;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @JoinColumn(name = "user_info_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    UserInfo userInfo;
+    @JoinColumn(name = "course_post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    CoursePost coursePost;
+
+    @Builder
+    public Bookmark(
+        final UserInfo userInfo,
+        final CoursePost coursePost
+    ) {
+        this.userInfo = userInfo;
+        this.coursePost = coursePost;
+    }
+}
