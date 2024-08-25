3eb88173ced2883bd852f85c0bfec5664e24d6b3
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 23:37:54 +0900

    feature : 에러 코드 추가

--- src/main/java/com/swart/runwith/domain/bookmark/exception/BookmarkErrorCode.java
+++ src/main/java/com/swart/runwith/domain/bookmark/exception/BookmarkErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum BookmarkErrorCode implements ErrorCode {
     // 400
-    ALREADY_BOOKMARK(HttpStatus.BAD_REQUEST, "이미 북마크한 글입니다.");
+    ALREADY_BOOKMARK(HttpStatus.BAD_REQUEST, "이미 북마크한 글입니다."),
+
+    // 404
+    NOT_FOUND_BOOKMARK(HttpStatus.NOT_FOUND, "존재하지 않는 북마크입니다.");
 
     private final HttpStatus status;
     private final String message;
