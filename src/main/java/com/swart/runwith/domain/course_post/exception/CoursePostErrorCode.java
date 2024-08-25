84c1791216835d3b89507d8b5339b3d0f614c472
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 19:38:11 +0900

    feature : 에러코드 작성

--- src/main/java/com/swart/runwith/domain/course_post/exception/CoursePostErrorCode.java
+++ src/main/java/com/swart/runwith/domain/course_post/exception/CoursePostErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum CoursePostErrorCode implements ErrorCode {
+    // 401
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "해당 코스 공유 글에 접근 권한이 없습니다."),
     // 404
     NOT_FOUND_COURSE_POST(HttpStatus.NOT_FOUND, "코스 공유 글을 찾을 수 없습니다."),
     ;
