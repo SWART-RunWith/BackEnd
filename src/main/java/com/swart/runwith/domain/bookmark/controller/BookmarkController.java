ea3fd3e96e912583d200162fcf5606df4cf0d656
shin0112 <wnslcosltimo12@gmail.com>
2024-08-25 23:16:09 +0900

    feature : controller - 코스 공유 글 북마크

--- src/main/java/com/swart/runwith/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/com/swart/runwith/domain/bookmark/controller/BookmarkController.java
@@ -1,7 +1,12 @@
 package com.swart.runwith.domain.bookmark.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import com.swart.runwith.domain.bookmark.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+
+    @PostMapping("/courses/{course_id}/bookmark")
+    public ResponseEntity<?> create(
+        @PathVariable(name = "course_id") Long courseId
+    ) {
+        bookmarkService.create(courseId);
+
+        return ResponseEntity
+            .status(OK)
+            .build();
+    }
 }
