81f8941504650844393a3b8975e74b58cdd08031
shin0112 <wnslcosltimo12@gmail.com>
2024-09-01 17:13:57 +0900

    feature : mapper 작성

---
+++ src/main/java/com/swart/runwith/domain/bookmark/mapper/BookmarkEntityMapper.java
@@ -0,0 +1,10 @@
+package com.swart.runwith.domain.bookmark.mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = SPRING)
+public interface BookmarkEntityMapper {
+
+}
