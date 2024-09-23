663697898be14f25beb241b074ea3b11d413ddbd
YeojoonHong <hyjhong2002@naver.com>
2024-09-23 13:35:22 +0900

    feature : 러닝 데이터 entity mapper 생성

---
+++ src/main/java/com/swart/runwith/domain/running_data/mapper/RunningDataEntityMapper.java
@@ -0,0 +1,10 @@
+package com.swart.runwith.domain.running_data.mapper;
+
+import org.mapstruct.Mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface RunningDataEntityMapper {
+
+}
