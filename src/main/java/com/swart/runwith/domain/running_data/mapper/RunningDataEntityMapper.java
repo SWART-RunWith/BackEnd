ba80d6676caaab58672689f2a8d2a63a135f604c
YeojoonHong <hyjhong2002@naver.com>
2024-10-01 12:15:14 +0900

    feature : 러닝 데이터 entity mapper 작성

--- src/main/java/com/swart/runwith/domain/running_data/mapper/RunningDataEntityMapper.java
+++ src/main/java/com/swart/runwith/domain/running_data/mapper/RunningDataEntityMapper.java
@@ -1,10 +1,22 @@
 package com.swart.runwith.domain.running_data.mapper;
 
+import com.swart.runwith.domain.running_data.dto.service.request.RunningDataCreateServiceRequestDto;
+import com.swart.runwith.domain.running_data.dto.service.response.RunningDataReadServiceResponseDto;
+import com.swart.runwith.domain.running_data.entity.RunningData;
+import com.swart.runwith.domain.user.entity.UserInfo;
+import org.apache.catalina.User;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(componentModel = SPRING)
 public interface RunningDataEntityMapper {
-
+    RunningData toRunningData(
+        RunningDataCreateServiceRequestDto serviceRequestDto,
+        UserInfo userInfo
+    );
+    
+    RunningDataReadServiceResponseDto toRunningDataReadServiceResponseDto(
+        RunningData runningData
+    );
 }
