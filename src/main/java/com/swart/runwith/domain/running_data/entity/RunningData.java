a9c6c3095e8986cb6f251efb9f083693f819c8ea
shin0112 <wnslcosltimo12@gmail.com>
2024-10-02 22:36:34 +0900

    fix : 러닝 데이터 merge conflict 해결

--- src/main/java/com/swart/runwith/domain/running_data/entity/RunningData.java
+++ src/main/java/com/swart/runwith/domain/running_data/entity/RunningData.java
@@ -1,30 +1,21 @@
 package com.swart.runwith.domain.running_data.entity;
 
 import com.swart.runwith.domain.running_data.dto.service.request.RunningDataUpdateServiceRequestDto;
-import com.swart.runwith.domain.running_shoes.dto.service.request.RunningShoesUpdateServiceRequestDto;
-import com.swart.runwith.domain.running_shoes.entity.RunningShoes;
-
 import com.swart.runwith.domain.user.entity.UserInfo;
 import com.swart.runwith.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.FetchType;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import jakarta.persistence.Table;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.mapstruct.control.MappingControl.Use;
 
 
 @Entity
@@ -32,7 +23,7 @@ import org.mapstruct.control.MappingControl.Use;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RunningData extends BaseEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -47,7 +38,7 @@ public class RunningData extends BaseEntity {
     @JoinColumn(name = "user_info_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     UserInfo userInfo;
-    
+
     @Builder
     public RunningData(
         final Integer distance,
@@ -62,54 +53,14 @@ public class RunningData extends BaseEntity {
         this.course_url = course_url;
         this.userInfo = userInfo;
     }
-    
-    
+
     public void update(final RunningDataUpdateServiceRequestDto serviceRequestDto) {
         this.distance =
             serviceRequestDto.distance() != null ? serviceRequestDto.distance() : 0;
         this.time = serviceRequestDto.time() != null ? serviceRequestDto.time() : 0;
-        this.averagePace = serviceRequestDto.averagePace() != null ? serviceRequestDto.averagePace() : 0.0F;
-        this.course_url = serviceRequestDto.courseUrl() != null ? serviceRequestDto.courseUrl() : this.course_url;
+        this.averagePace =
+            serviceRequestDto.averagePace() != null ? serviceRequestDto.averagePace() : 0.0F;
+        this.course_url =
+            serviceRequestDto.courseUrl() != null ? serviceRequestDto.courseUrl() : this.course_url;
     }
- //    다른 테이블이랑 연관 관계를 사용할 때는 OneToMany, ManyToMany 등 연관 관계 어노테이션 사용해야 함
-//    여기서는 회원, 러닝화 등이 연관 관계 매핑
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    UserInfo userInfo; // 회원
-    @ManyToOne
-    @JoinColumn(name = "running_shoes")
-    RunningShoes runningShoes; // 러닝화
-    @Column
-    Integer distance = 0; // 거리
-    @Column
-    Integer time = 0; // 시간
-    @Column
-    Float averagePace = 0.0f; // 평균 페이스
-    @Column
-    String courseUrl; // 코스
-    
-    @Builder
-    public RunningData(
-        final UserInfo userInfo,
-        final RunningShoes runningShoes,
-        final Integer distance,
-        final Integer time,
-        final Float averagePace,
-        final String courseUrl
-    ) {
-        this.userInfo = userInfo;
-        this.runningShoes = runningShoes;
-        this.distance = distance;
-        this.time = time;
-        this.averagePace = averagePace;
-        this.courseUrl = courseUrl;
-    }
-    
-//    public void update(final RunningDataUpdateServiceRequestDto serviceRequestDto, RunningShoes runningShoes) {
-//        this.runningShoes = runningShoes;
-//        this.distance = serviceRequestDto.distance();
-//        this.time = serviceRequestDto.time();
-//        this.averagePace = serviceRequestDto.averagePace();
-//        this.courseUrl = serviceRequestDto.courseUrl();
-//    }
 }
